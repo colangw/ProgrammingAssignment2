f15db7d493959c7e7635dabad16b2f94e8ec853b
Werner Colangelo <wernercolangelo@gmail.com>
2014-09-19 09:24:44 -0700

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Coursera: R Programming
+## Werner Colangelo
 
 ## Write a short comment describing this function
 
